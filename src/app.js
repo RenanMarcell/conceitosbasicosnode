e249332bd40a66f34988fa6c8e8bc47760f94b88
renanpc <renan.vieira@luizalabs.com>
2020-04-21 21:52:22 -0300

    upload solution

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,64 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+const validate_repository = (request, response, next) => {
+    if (
+        !'title' in request.body
+        || !'url' in request.body
+        || !'techs' in request.body
+    ) {
+        return response.status(400).json(
+            { 'error': 'Repository missing required fields'}
+        );
+    }
+    next();
+}
+
+const find_repository = (req, res, next) => {
+    let repository_found = false;
+    repositories.forEach((project, idx) => {
+        if (project.id === req.params.id) {
+            req.index = idx;
+            repository_found = true;
+        }
+    });
+
+    if (!repository_found) return res.status(400).json(
+        { 'error': 'Repository not found'}
+    );
+
+    return next();
+};
+
+app.get("/repositories", (request, response) => response.send(repositories));
+
+app.post("/repositories", validate_repository, (request, response) => {
+    const { title, url, techs } = request.body;
+    const repository = {
+        title,
+        url,
+        techs,
+        likes: 0,
+        id: uuid()
+    }
+    repositories.push(repository);
+    return response.status(202).send(repository);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.put("/repositories/:id", find_repository, (request, response) => {
+    const { title, url, techs } = request.body;
+    repositories[request.index] = { ...repositories[request.index], title, url, techs }
+    return response.status(200).send(repositories[request.index]);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", find_repository, (request, response) => {
+    repositories.splice(request.index, 1);
+    return response.status(204).send();
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", find_repository, (request, response) => {
+    repositories[request.index].likes++;
+    return response.status(200).send(repositories[request.index]);
 });
 
 module.exports = app;
